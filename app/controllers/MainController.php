0838a0b3410ab23bc30a13e11f44da583e68fa36
Anton Zhary <anton.zhary@gmail.com>
2020-03-04 20:26:23 +0300

    add clas Controller

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,10 +2,11 @@
 
 namespace app\controllers;
 
-class MainController
+class MainController extends AppController
 {
     public function indexAction()
     {
+        debug($this->route);
         echo __METHOD__;
     }
 }

778676406f77451d36f920f6e261ec1cebb64fbd
Anton Zhary <anton.zhary@gmail.com>
2020-04-26 01:04:14 +0300

    📦 NEW: Add Related products

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends AppController
         // Хлебные крошки
 
         // связанные товары
-
+        $related = \R::getAll("SELECT * from related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
         // запись в куки запрошенного товара
 
         // просмотренные товары
@@ -27,7 +27,7 @@ class ProductController extends AppController
         // модификации
 
         $this->setMeta($product->title, $product->description, $product->keywords );
-        $this->set(compact('product'));
+        $this->set(compact('product', 'related'));
 
 
     }

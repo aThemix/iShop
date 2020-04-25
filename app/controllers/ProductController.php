1cd6a8e7e185c11c53c632d68191230bdd1270d1
Anton Zhary <anton.zhary@gmail.com>
2020-04-26 00:41:40 +0300

    📦 NEW: Add Product card

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace app\controllers;
+
+
+class ProductController extends AppController
+{
+    public function viewAction()
+    {
+        $alias = $this->route['alias'];
+        $product = \R::findOne('product', "alias = ? AND status = '1'", [$alias]);
+        if(!$product){
+            throw new \Exception('Страница не найдена', 404);
+        }
+
+        // Хлебные крошки
+
+        // связанные товары
+
+        // запись в куки запрошенного товара
+
+        // просмотренные товары
+
+        // галлерея
+
+        // модификации
+
+        $this->setMeta($product->title, $product->description, $product->keywords );
+        $this->set(compact('product'));
+
+
+    }
+}
